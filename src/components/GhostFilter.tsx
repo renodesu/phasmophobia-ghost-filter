c44c6f50dd777a312534fe19772746e73933a3c4
Harri Virtanen <harri@niodi.fi>
2021-09-17 00:33:50 +0300

    Fix typo

--- src/components/GhostFilter.tsx
+++ src/components/GhostFilter.tsx
@@ -82,7 +82,7 @@ const GhostFilter = () => {
         <div>
           <h3>Usage</h3>
           <p>Use confirmed evidence to narrow down the ghost type.</p>
-          <p>Use excluded evidence to further filter the ghost type if you sure it can't be some specific evidence.</p>
+          <p>Use excluded evidence to further filter the ghost type if you're sure it can't be some specific evidence.</p>
           <p>
             <span className={styles.greenWord}>Green</span> highlight means that evidence is missing to identify that specific ghost.
           </p>
