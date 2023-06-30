6289a6cbdc5a00a5948415fb796600b2e4f559af
Harri Virtanen <harri@niodi.fi>
2023-06-30 19:23:55 +0300

    Fix tracking

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx'
 import { useRecoilValue } from 'recoil'
 
 import '../styles/global.scss'
-import { useUmamiTracking } from '../hooks/useUmamiTracking'
 import { colorSchemeState } from '../utils/state'
 
 import Header from './Header'
@@ -11,8 +10,6 @@ import MainContent from './MainContent'
 const App = () => {
   const isDarkMode = useRecoilValue(colorSchemeState)
   const colorScheme = isDarkMode ? 'dark' : 'light'
-
-  useUmamiTracking()
 
   return (
     <div id="app" className={clsx(colorScheme, 'min-h-screen')}>
