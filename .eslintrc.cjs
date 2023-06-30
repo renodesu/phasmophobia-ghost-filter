c1d902e7d4d4fad5f95f4a3fb57d0f80e6d4df81
Harri Virtanen <harri@niodi.fi>
2023-06-30 19:28:08 +0300

    Upgrade deps again

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,15 @@
 module.exports = {
-  extends: '@renodesu/eslint-config',
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    '@renodesu/eslint-config',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import svgLoader from 'vite-plugin-svgr'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgLoader()],
+  plugins: [react(), svgr()],
 })
